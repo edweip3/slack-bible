443db5115170aa5ac640ae376002e2811aa86230
Ed Wei <ed@mappedin.ca>
2016-10-01 20:50:33 -0400

    tagging some books and make slack happy

--- server.js
+++ server.js
@@ -16,54 +16,66 @@ server.route({
 	path: '/search',
 	handler: function (request, reply) {
 
-		var temp = request.query.text.split(' ');
-		var book = '';
-		if (temp[0]==='1' || temp[0]==='2' || temp[0]==='3') {
-			book = temp[0] + ' ' + temp[1];
-		} else {
-			book = temp[0];
-		}
+		try {
+			var temp = request.query.text.split(' ');
+			var temp2;
 
-		if (!temp[1]){
-			return reply();
-		}
-		var temp2 = temp[1].split(':');
-		var chapter = temp2[0];
-		if (!temp2[1]){
-			return reply();
-		}
-		var temp3 = temp2[1].split('-');
+			var book = '';
+			if (temp[0]==='1' || temp[0]==='2' || temp[0]==='3') {
+				book = temp[0] + ' ' + temp[1];
+				temp2 = temp[2].split(':');
 
-		if (!temp3[0]) {
-			return reply();
-		}
+			} else {
+				book = temp[0];
+				temp2 = temp[1].split(':');
+			}
 
-		var min = parseInt(temp3[0]);
-		var max = temp3[1] ? parseInt(temp3[1]) : parseInt(temp3[0]);
+			//console.log(book);
 
-		var verses = [];
-		for (var i=min; i<=max; i++) {
-			verses.push(i);
-		}
-
-		var ret = '';
-		async.eachSeries(verses, function(verse, cb){
-			Bible.getVerse(book, chapter, verse, function(err, result){
-				if (err) {
-					console.log(err);
-					return cb(err);
-				}
-	        	ret += verse + '. ' + result + '\n';
-	        	cb();
-			});
-		}, function(err) {
-			if (err) {
-				console.log(err);
-				//return reply(Boom.badRequest('sorry'));
+			if (!temp[1]){
 				return reply();
 			}
-			reply(ret);
-		});
+			var chapter = temp2[0];
+			if (!temp2[1]){
+				return reply();
+			}
+			var temp3 = temp2[1].split('-');
+
+			if (!temp3[0]) {
+				return reply();
+			}
+
+			var min = parseInt(temp3[0]);
+			var max = temp3[1] ? parseInt(temp3[1]) : parseInt(temp3[0]);
+
+			var verses = [];
+			for (var i=min; i<=max; i++) {
+				verses.push(i);
+			}
+
+			var ret = '';
+			async.eachSeries(verses, function(verse, cb){
+				Bible.getVerse(book, chapter, verse, function(err, result){
+					if (err) {
+						console.log(err);
+						return cb(err);
+					}
+		        	ret += verse + '. ' + result + '\n';
+		        	cb();
+				});
+			}, function(err) {
+				if (err) {
+					console.log(err);
+					//return reply(Boom.badRequest('sorry'));
+					return reply();
+				}
+				reply(ret);
+			});
+
+		} catch(e) {
+			console.log(e);
+			reply();
+		}
 
     },
     config: {
